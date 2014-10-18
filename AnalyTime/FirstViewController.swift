a3e7004555e7623ee6ab0e06de7f2615868d0a63
Jerry <czwjerry@gmail.com>
2014-10-18 01:02:46 -0500

    first

--- AnalyTime/FirstViewController.swift
+++ AnalyTime/FirstViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import EventKit
+import EventKitUI
+
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        //accessing the event store
+        var eventStore = EKEventStore();
+        eventStore.requestAccessToEntityType(EKEntityTypeEvent) {
+            (success: Bool, error: NSError!) in
+            println("Got permission = \(success); error = \(error)")
+        }
+        
+        //accessing calendars, we use "nil" below means all calendars
+        var eventCalendars = eventStore.calendarsForEntityType(EKEntityMaskEvent) as [EKCalendar]
+        
+        //accessing events
+        var startDate = NSDate().dateByAddingTimeInterval(-999998);
+        var endDate = NSDate();
+        var predicate = eventStore.predicateForEventsWithStartDate(startDate, endDate: endDate, calendars: nil);
+        var events = eventStore.eventsMatchingPredicate(predicate);
+    
+        var event = events[3] as EKEvent
+        println(event.startDate)
+        println(event.endDate)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
